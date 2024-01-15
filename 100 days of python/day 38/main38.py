8f209c9632ffefba99b32859bf3a49ca56057304
AnenIsaac <anenbarneyisaac@gmail.com>
2024-01-15 23:59:40 +0300

    more work done

--- 100 days of python/day 38/main38.py
+++ 100 days of python/day 38/main38.py
@@ -4,6 +4,7 @@ APPID = "cfc758ec"
 APIKEYS = "ae7b0c777a53bbb111e8e6bce7c7cfc5"
 endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 endpoint_calories = "ae7b0c777a53bbb111e8e6bce7c7cfc5"
+sheets_url ='https://api.sheety.co/db576b61a317675d114004c8294a6961/myWorkouts/workouts'
 
 user_weight = 74
 user_height = 183
@@ -16,10 +17,11 @@ header = {
 }
  
 params = {
-    "query" : user_input, 
-    "weight_kg" : user_weight, 
-    "height_cm" : user_height,
-    "age" : user_age
+    "query": user_input,  # Include the user's input as the "query" parameter
+    "weight_kg": user_weight,
+    "height_cm": user_height,
+    "age": user_age,
 }
-response = requests.post(url=endpoint, headers=header, params=params)
+
+response = requests.post(url=endpoint, headers=header, json=params)
 print(response.text)
