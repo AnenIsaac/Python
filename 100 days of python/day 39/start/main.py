a31290b28e97e7040986ea299af666010d7f43aa
AnenIsaac <anenbarneyisaac@gmail.com>
2024-01-19 23:25:58 +0300

    main.py updated

--- 100 days of python/day 39/start/main.py
+++ 100 days of python/day 39/start/main.py
@@ -1,1 +1,30 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from flight_data import FlightData
+from notification_manager import NotificationManager
+
+ORIGIN_CITY_IATA = "LON"
+
+data_manager = DataManager()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+sheet_data = data_manager.get_data()
+
+if sheet_data[0]["iataCode"] == "":
+    city_names = [row["city"] for row in sheet_data]
+    codes = flight_search.get_destination_codes(city_names)
+    data_manager.update_codes(codes)
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_months_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for row in sheet_data:
+    flight = flight_search.search_flights(
+        ORIGIN_CITY_IATA,
+        row["iataCode"],
+        from_time=tomorrow,
+        to_time=six_months
