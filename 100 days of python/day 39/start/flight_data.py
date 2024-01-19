25779a882a69ff2c9673029c238fa9a242308777
AnenIsaac <anenbarneyisaac@gmail.com>
2024-01-19 23:24:34 +0300

    flight_data.py

--- 100 days of python/day 39/start/flight_data.py
+++ 100 days of python/day 39/start/flight_data.py
@@ -1,3 +1,14 @@
+# class FlightData:
+#     #This class is responsible for structuring the flight data.
+#     pass
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    # This class is responsible for structuring the flight data.
+    def __init__(self, price, origin_city, origin_airport_code, destination_city, destination_airport_code, out_date, return_date):
+        self.price = price
+        self.origin_city = origin_city
+        self.origin_airport_code = origin_airport_code
+        self.destination_city = destination_city
+        self.destination_airport_code = destination_airport_code
+        self.out_date = out_date
+        self.return_date = return_date
